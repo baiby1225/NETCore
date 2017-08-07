a3429672f47715218659198c42e5278ce8776c10
baiby1225 <baiby1225@163.com>
2017-08-07 22:54:42 +0800

    create

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = true;
+            while (b)
+            {
+                Console.WriteLine("Hello World!,input:");
+                string input = Console.ReadLine();
+                if (input=="y")
+                {
+                    b = false;
+                }
+                Console.WriteLine("你输入了："+input);
+            }
+          
+
+           
+        }
+    }
+}
